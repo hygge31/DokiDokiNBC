b614f9a821654375570008c3dd802a688a9f7d1d
Orknology <kjunha11@gmail.com>
2024-02-08 21:03:17 +0900

    Feat - 플레이어 상하좌우 움직임 추가
    InputSystem과 움직임 기능만 포함된 컨트롤러 코드 추가

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    private Vector2 curMovementInput;
+
+    private Rigidbody2D _rigidbody;
+
+    public static PlayerController instance;
+
+    private void Awake()
+    {
+        instance = this;
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 dir = transform.up * curMovementInput + transform.right * curMovementInput;
+        dir *= moveSpeed;
+
+        _rigidbody.velocity = dir;
+    }
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            curMovementInput = Vector2.zero;
+        }
+    }
+}
