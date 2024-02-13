74bd7178bb3b9ec5692c1fc4e501e442b0e6cebb
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-02-13 20:02:46 +0900

    Feat - GetDamaged Action 추가

--- Assets/Scripts/GJY_Scripts/Managers/Contents/PlayerStatManager.cs
+++ Assets/Scripts/GJY_Scripts/Managers/Contents/PlayerStatManager.cs
@@ -8,12 +8,16 @@ public class PlayerStatManager
     public Action OnPlayerSetup;
     public Action OnHealing;
     public Action OnExtraHealing;
+    public Action OnGetDamaged;
     public Action<Item_SO> OnApplyPerkStat;
-    public Action OnGetOrChangeActiveItem;
+    public Action<Item_SO> OnGetOrChangeActiveItem;
 
+    // ### HPs
     private int _hp;
     private int _maxHp;
     private int _extraHp;
+
+    // ### Stats
     private float _atk;
     private float _fireRate;
     private float _crit;
@@ -22,6 +26,7 @@ public class PlayerStatManager
     public int Hp => _hp;
     public int MaxHp => _maxHp;
     public int ExtraHp => _extraHp;
+
     public float Atk => _atk;
     public float FireRate => _fireRate;
     public float Crit => _crit;
@@ -60,11 +65,21 @@ public class PlayerStatManager
     public void GetActiveItem(Item_SO item)
     {
         ActiveItem = item;
-        OnGetOrChangeActiveItem?.Invoke();
+        OnGetOrChangeActiveItem?.Invoke(item);
     }
 
-    public void GetDamage()
+    public void GetDamaged()
     {
+        if(_extraHp > 0)
+            _extraHp--;
+        else
+            _hp--;
 
+        OnGetDamaged?.Invoke();
+
+        if(_hp <= 0)
+        {
+            //To Do - GameOver
+        }
     }
 }
