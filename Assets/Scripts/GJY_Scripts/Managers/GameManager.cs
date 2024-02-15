a93c169877f33c45e399b8b8b119136c39f7a5a9
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-02-16 08:59:21 +0900

    Chore - 게임 시작 단계 수정

--- Assets/Scripts/GJY_Scripts/Managers/GameManager.cs
+++ Assets/Scripts/GJY_Scripts/Managers/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager
 {
     public Action OnGetCodePiece;
 
-    public int day = 5;
+    public int day = 3;
 
     public int CodePiece { get; private set; } = 0;
     public bool IsPCPowerOff { get; private set; } = false;
