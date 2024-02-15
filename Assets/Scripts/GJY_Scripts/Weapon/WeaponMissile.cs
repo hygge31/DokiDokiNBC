f190f0761238727033e2fd015cf22fe19d1fb4ee
Orknology <kjunha11@gmail.com>
2024-02-16 02:25:39 +0900

    Feat - 미사일 발사체 증가x2 / 파이어볼 발사체 증가+발사각 추가+관통기능 추가+관통소리 추가

--- Assets/Scripts/GJY_Scripts/Weapon/WeaponMissile.cs
+++ Assets/Scripts/GJY_Scripts/Weapon/WeaponMissile.cs
@@ -6,7 +6,7 @@ public class WeaponMissile : IWeapon
 {
     public void Attack(PlayerStatManager playerStat, Vector2 origin, Vector2 dir)
     {
-        for(int i = 0; i< 10; i++)
+        for(int i = 0; i< 10 + (playerStat.A_AddBullet * 2); i++)
         {
             Bullet_Missile missile = Managers.RM.Instantiate("Projectiles/Bullet_Missile").GetComponent<Bullet_Missile>();
             missile.Setup(origin, dir, playerStat.A_Atk, playerStat.W_BulletSpeed, playerStat.W_Duration, playerStat.A_PierceCount);
--- Assets/Scripts/GJY_Scripts/Weapon/WeaponNormal.cs
+++ Assets/Scripts/GJY_Scripts/Weapon/WeaponNormal.cs
@@ -5,8 +5,33 @@ using UnityEngine;
 public class WeaponNormal : IWeapon
 {
     public void Attack(PlayerStatManager playerStat, Vector2 origin, Vector2 dir)
-    {        
-        Bullet_Fireball fireball = Managers.RM.Instantiate("Projectiles/Bullet_Fireball").GetComponent<Bullet_Fireball>();
-        fireball.Setup(origin, dir, playerStat.A_Atk, playerStat.W_BulletSpeed, playerStat.W_Duration, playerStat.A_PierceCount);
+    {
+        float initialAngle = -15f * (playerStat.A_AddBullet / 2f);
+
+        if(playerStat.A_AddBullet == 0)
+        {
+            // 한 발만 발사할 때
+            Bullet_Fireball onefireball = Managers.RM.Instantiate("Projectiles/Bullet_Fireball").GetComponent<Bullet_Fireball>();
+            onefireball.Setup(origin, dir, playerStat.A_Atk, playerStat.W_BulletSpeed, playerStat.W_Duration, playerStat.A_PierceCount);
+        }
+        
+
+        // 한 발 이상 발사
+        for (int i = 0; i <= playerStat.A_AddBullet; i++)
+        {
+            // 투사체 간 거리 각도 계산
+            float angle = initialAngle + i * 15f;
+
+            // 회전된 방향으로 투사체를 생성합니다.
+            Vector2 newDir = RotateVector2(dir, angle);
+            Bullet_Fireball fireball = Managers.RM.Instantiate("Projectiles/Bullet_Fireball").GetComponent<Bullet_Fireball>();
+            fireball.Setup(origin, newDir, playerStat.A_Atk, playerStat.W_BulletSpeed, playerStat.W_Duration, playerStat.A_PierceCount);
+        }
+    }
+
+    // 벡터를 회전하는 메서드입니다.
+    private static Vector2 RotateVector2(Vector2 v, float degree)
+    {
+        return Quaternion.Euler(0, 0, degree) * v;
     }
 }
